c9ca6a4e7f889ac303c4015920a7a67077d62fd5
Zane Sharif <Zane.Sharif@gmail.com>
2022-06-30 20:45:42 -0700

    All criteria met

--- assets/script/scoreboard.js
+++ assets/script/scoreboard.js
@@ -2,8 +2,11 @@ var highScoresList = document.querySelector('#highScoresList')
 var scores = JSON.parse(localStorage.getItem('highScores')) || []
 
 var leaders = localStorage.getItem('submission')
+var scoreList =document.querySelector('#scoreList')
 
+function showLeader () {
+    scoreList.textContent = leaders
+    
+};
 
-if (username){
-    Text.textContent = leaders
-}
+showLeader();
