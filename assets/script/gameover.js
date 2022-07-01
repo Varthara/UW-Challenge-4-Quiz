89c874c188b5b008a95ba5317573972b095aa0df
Zane Sharif <Zane.Sharif@gmail.com>
2022-06-30 21:17:05 -0700

    removed some code from gameover.js that saves score

--- assets/script/gameover.js
+++ assets/script/gameover.js
@@ -3,6 +3,8 @@ var ScoreSaving = document.querySelector('#saveScoreBtn')
 var EndScore = document.querySelector('#finalScore')
 var mostRecentScore = localStorage.getItem('mostRecentScore')
 
+EndScore.innerHTML = mostRecentScore
+
 ScoreSaving.addEventListener("click", function(event) {
     event.preventDefault();
 
@@ -18,29 +20,9 @@ var userHighScore = JSON.parse(localStorage.getItem('highScores'))
 
 const perfectRun = 50
 
-EndScore.innerHTML = mostRecentScore
 
-savescores = e => {
-    e.preventDefault()
 
-    let score = {
-        score: mostRecentScore,
-        name: username.value 
-    }
 
-    userHighScore.push(score)
-
-    userHighScore.sort((a,b) => {
-        return b.score -a.score 
-    })
-
-    userHighScore(50)
-
-    localStorage.setItem('highScores', JSON.stringify(userHighScore))
-    window.location.assign('/')
-    
-
-}
 
 
 saveScoreBtn.addEventListener('click', function () {})
