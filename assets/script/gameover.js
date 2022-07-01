e77900ca1727669ad6382440a6f891cbd74b6161
Zane Sharif <Zane.Sharif@gmail.com>
2022-06-30 20:24:43 -0700

    so close, name and score are saving to local storage. Now to get it to appear on leaderboard

--- assets/script/gameover.js
+++ assets/script/gameover.js
@@ -1,7 +1,18 @@
-var username = document.querySelector('#username')
+const username = document.querySelector('#username')
 var ScoreSaving = document.querySelector('#saveScoreBtn')
 var EndScore = document.querySelector('#finalScore')
 var mostRecentScore = localStorage.getItem('mostRecentScore')
+
+ScoreSaving.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var submission = {
+        initial: username.value,
+        recentscore: mostRecentScore
+    }
+    localStorage.setItem("submission", JSON.stringify(submission));
+    
+});
 
 var userHighScore = JSON.parse(localStorage.getItem('highScores'))
 
--- assets/script/scoreboard.js
+++ assets/script/scoreboard.js
@@ -1,8 +1,9 @@
 var highScoresList = document.querySelector('#highScoresList')
 var scores = JSON.parse(localStorage.getItem('highScores')) || []
 
+var leaders = localStorage.getItem('submission')
 
-highScoresList.innerHTML = 
-userHighScore.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join('')
+
+if (username){
+    Text.textContent = leaders
+}
